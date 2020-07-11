b37462dc0f72441bc58d02488e8fdc1c85f2feaa
Avhishek Adhikary <avhishe.adhikary11@gmail.com>
2020-07-11 23:10:00 +0530

    Added Application Icons

--- app/src/main/java/com/example/med_it_yourself/UserAmbulanceMapActivity.java
+++ app/src/main/java/com/example/med_it_yourself/UserAmbulanceMapActivity.java
@@ -24,6 +24,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.model.MapStyleOptions;
